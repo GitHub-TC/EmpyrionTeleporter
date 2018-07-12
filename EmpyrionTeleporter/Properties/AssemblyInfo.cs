9d0893c5161231a5142043168beee1a50b430590
ASTIC <ASTIC@hotmail.de>
2018-07-12 15:42:53 +0200

    Assemblyinfo angepasst

--- EmpyrionTeleporter/Properties/AssemblyInfo.cs
+++ EmpyrionTeleporter/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("EmpyrionTeleporter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ASTIC/TC")]
 [assembly: AssemblyProduct("EmpyrionTeleporter")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
